55acafc58ebe50f2623246c78943aa8031528e32
yopiangga <yopigambyok@gmail.com>
2020-12-05 19:52:49 +0700

    Login sign up berhasil untuk desktop

--- js/style-main.js
+++ js/style-main.js
@@ -7,6 +7,22 @@ $(document).ready(function(){
     });
 
 
+    $('.sign-in-sign-up .content .content-right .sign-up .btn-sign-up').click(function(){
+        $('.sign-in-sign-up .content').toggleClass('active');
+
+        $('.sign-in-sign-up .content .content-left .sign-in').addClass('animate__animated animate__fadeInRight');
+        $('.sign-in-sign-up .content .content-right .sign-up-2').addClass('animate__animated animate__fadeInLeft');
+    });
+
+    $('.sign-in-sign-up .content .content-left .sign-in-2 .btn-sign-in').click(function(){
+        $('.sign-in-sign-up .content').toggleClass('active');
+
+        $('.sign-in-sign-up .content .content-right .sign-up').addClass('animate__animated animate__fadeInLeft');
+        $('.sign-in-sign-up .content .content-left .sign-in-2').addClass('animate__animated animate__fadeInRight')
+
+    });
+
+    
 });
 
 
